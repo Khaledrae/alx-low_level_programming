8962731243d0bbf30cf23bc9cfa239273043ffe9
Khaledrae <educatejulius@gmail.com>
2023-10-08 16:57:28 -0700

    Kumbuka to include libraries

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,10 +11,11 @@
 int main(void)
 {
 int n;
+int last_digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-int last_digit = n % 10;
+last_digit = n % 10;
 if (last_digit > 5)
 printf("Last digit of %i is greater than 5\n", n);
 else if (last_digit == 0)
