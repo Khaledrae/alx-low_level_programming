55fed6da32cecb85b3bf5b924cbaaadbefaf70b0
Khaledrae <educatejulius@gmail.com>
2023-10-08 07:10:24 -0700

    Cheki from CMD4

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
-/* To be able to generate a random number */
-#include<stdlib.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
 /**
  * main - Entry point
  *
@@ -9,6 +11,9 @@
 int main(void)
 {
 int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
 if (n > 0)
 printf("%i is positive\n", n);
 else if (n < 0)
