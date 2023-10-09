1c54ee7bd4dc0ed4f4ac5905281892e539b28f36
Khaledrae <educatejulius@gmail.com>
2023-10-08 17:40:21 -0700

    Wewe4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+/* To print all small letters */
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+char letter = 'a';
+while (letter <= 'z'&& (letter != 'q' OR letter != 'e'))
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+
+return (0);
+}
