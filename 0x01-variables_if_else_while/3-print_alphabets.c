6797d720174b0776f66c67d3e523a534d326790e
Khaledrae <educatejulius@gmail.com>
2023-10-08 17:36:24 -0700

    Wewe3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Prints letters in Smal then caps boy
+ *
+ * Return: 0 Always(Success)
+ */
+int main(void)
+{
+char smallz = 'a';
+char biggz = 'A';
+while (biggz <= 'Z')
+{
+putchar(smallz);
+puthar(biggs)
+smallz++;
+biggz++;
+}
+puthar('\n');
+
+return (0);
+}
