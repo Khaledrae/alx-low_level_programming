467afa474a5b639ffcf1cf5c842a5501ebce716f
Khaledrae <educatejulius@gmail.com>
2023-10-08 17:13:35 -0700

    Kumbuka to include libraries1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+/* To print all small letters */
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ * main Entry Point
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+putchar("%s", letter);
+}
+putchar("\n");
+
+return (0);
+}
