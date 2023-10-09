a1b07d27a17ad6e05a2cfd6051d92a15bb358e9e
Khaledrae <educatejulius@gmail.com>
2023-10-09 00:30:51 -0700

    Ebu tone

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,8 +9,8 @@ int main(void)
 printf("Size of a char: %d  byte(s)\n", sizeof(char));
 printf("Size of an int: %i byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %lli byte(s)\n", sizeof(long long int));
-printf("Size of a float: %d byte(s)\n", sizeof(float));
+printf("Size of a long long int: %li byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)", sizeof(float));
 
 return (0);
 }
