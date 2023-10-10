d482cebef406399bbc95945c4abd8b626793e242
Khaledrae <educatejulius@gmail.com>
2023-10-10 02:00:09 -0700

    weuwe

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,6 @@ printf("Size of an int: %i byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %li byte(s)\n", sizeof(long long int));
 printf("Size of a float: %d byte(s)", sizeof(float));
-
+write("Anything\n");
 return (0);
 }
