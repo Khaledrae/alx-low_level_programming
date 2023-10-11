e864b9b2246d3312d0c8b60bd850b5dc51ebe940
Khaledrae <educatejulius@gmail.com>
2023-10-11 11:29:38 -0700

    TEst

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include "main.c"
+/**
+ *main - Entry Point
+ *
+ *Return: (0) Always success
+ */
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(int i);
+
+return (0);
+}
