c344cd024454801ef9aee51cafc39a61f0faeb08
Khaledrae <educatejulius@gmail.com>
2023-10-11 16:03:36 -0700

    Wueh6

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  *_islower - Entry point
- *
+ *c: A random letter
  *Return: int Always
  */
 int _islower(int c)
 {
-if(c >= 'a' && c <= 'z')
+if (c >= 'a' && c <= 'z')
 {
 return (1);
 }
