0a7bfc9d5ca5d75c4bb93d23f7a296e6b30ad1ed
Khaledrae <educatejulius@gmail.com>
2023-10-11 16:26:32 -0700

    Thirrd Party?

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  *_isalpha - Entry point
- *@c: A random letter
+ *@n: A random character
  *Return: int Always
  */
-int _isalpha(int c)
+int _isalpha(int n)
 {
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+if ((n >= 'a' && n <= 'z') || (n >= 'A' && n <= 'Z'))
 {
 return (1);
 }
