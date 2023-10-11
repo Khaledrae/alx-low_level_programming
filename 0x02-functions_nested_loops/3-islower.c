b9188487256052384a83c2cac15591f61f5edc63
Khaledrae <educatejulius@gmail.com>
2023-10-11 15:32:54 -0700

    ewe

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - Entry point
+ *
+ *Return: void Always
+ */
+int main(void)
+{
+int c;
+if (_islower(c)==true)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
