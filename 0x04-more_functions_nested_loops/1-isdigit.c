0089f5c18563c2fefe20e2e4fc140f3df7bf16a2
Khaledrae <educatejulius@gmail.com>
2023-10-12 10:43:00 -0700

    Just Beninging4

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_isdigit - Checks for a number
+ *@c: The character to be checked
+ *Return: (0) Always
+ */
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
