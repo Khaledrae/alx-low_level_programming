321ad41db0c772adb21e489219107795dde34250
Khaledrae <educatejulius@gmail.com>
2023-10-11 18:55:45 -0700

    Team kumalo

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *void print_to_98 - Prints to 98 from a given number
+ *
+ *Return: Void
+ */
+void print_to_98(int n)
+{
+int n, i;
+if(n > 98)
+{
+for(i >=n; i>=98 i--)
+{
+_putchar('0'+i);
+}
+}
+else
+{
+for(i <= n; i >= 98; i++)
+{
+_putchar('0'+i);
+}
+}
+}
