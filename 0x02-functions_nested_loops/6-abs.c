84de93adc1832ac31de35faafc68a437bde9f72f
Khaledrae <educatejulius@gmail.com>
2023-10-11 17:08:05 -0700

    Wueh3

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_abs - Entry point
+ *@n: Number to get absolute for
+
+ *Return: Absolute number of passed figure
+ */
+int _abs(int n)
+{
+if (n > 0)
+{
+return (n);
+}
+else
+{
+return (-n);
+}
+}
