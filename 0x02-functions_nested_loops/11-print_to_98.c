b0ad7121fd6e8cc7f3f5c9a8d9d2bc4eba164ba3
Khaledrae <educatejulius@gmail.com>
2023-10-11 19:02:47 -0700

    Team Kufinish

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,14 +9,14 @@ void print_to_98(int n)
 int i;
 if(n > 98)
 {
-for(i >= n; i >= 98; i--)
+for(i = n; i >= 98; i--)
 {
 _putchar('0'+i);
 }
 }
 else
 {
-for(i <= n; i >= 98; i++)
+for(i = n; i <= 98; i++)
 {
 _putchar('0'+i);
 }
