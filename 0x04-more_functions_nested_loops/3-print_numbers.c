4c239dc6aae6fc44fa92f5dcb872ab1252fce53c
Khaledrae <educatejulius@gmail.com>
2023-10-12 11:06:56 -0700

    Haiyaa

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_most_numbers(void)
+ *
+ *
+ *Return: a void
+ */
+void print_most_numbers(void)
+{
+for (i = 0; i <= 9; i++ )
+{
+_putchar('0' + i);
+}
+_putchar('\n');
+}
