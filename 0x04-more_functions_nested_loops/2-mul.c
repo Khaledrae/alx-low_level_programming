9b38aa5432929636181188bc621c9f339c7e4678
Khaledrae <educatejulius@gmail.com>
2023-10-12 20:21:07 -0700

    Wererr

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,10 +3,12 @@
  *mul - entry point
  *@a: The first integer
  *@b: The last integer
+ *
  *Return: (0) Always
  */
 int mul(int a, int b)
 {
-int res = a * b;
+int res;
+res = a * b;
 return (res);
 }
