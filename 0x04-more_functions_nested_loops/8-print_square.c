c537146833704b9ec5d866400bb4c71613c3c165
Khaledrae <educatejulius@gmail.com>
2023-10-13 01:09:30 -0700

    Continuation

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ *void print_square - Prints a square based on given param
+ *@size: size of the square
+ *
+ *Return: void
+ */
+void print_square(int size)
+{
+int i, j;
+/* height */
+for (i = 0; i <= size; i++)
+{
+if (i == 0 || i ==size)
+{
+_putchar('|');
+}
+else
+{
+_putchar(' ');
+}
+/* Width */
+for (j = 0; j <= size; j++)
+{
+if (j == 0 || j ==size)
+{
+_putchar('-');
+}
+else
+{
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
