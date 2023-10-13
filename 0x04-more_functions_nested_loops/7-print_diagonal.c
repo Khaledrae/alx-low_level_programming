d4c10f382c5e23d9af584a9979c8a65853cc2f3c
Khaledrae <educatejulius@gmail.com>
2023-10-12 20:06:52 -0700

    Rush hour

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,9 +1,27 @@
 #include "main.h"
 /**
  *print_diagonal - prints a diaggonal line
- *
+ *@n: length of the line
+ *Retun:void
  */
 void print_diagonal(int n)
 {
-
+int i, j;
+if (n > 0)
+{
+for(i = 0; i < n; i++)
+{
+for(j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+if (i == n - 1)
+{
+contiue;
+}
+_putchar('\n');
+}
+}
+_putchar('\n');
 }
