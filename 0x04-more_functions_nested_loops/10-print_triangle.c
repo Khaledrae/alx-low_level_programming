28152803c26149b7bbc321b8f8aac00d168f051d
Khaledrae <educatejulius@gmail.com>
2023-10-13 04:45:32 -0700

    Let's see

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ *print_triangle - entry point
+ *@size: The size of the triangle
+ *
+ *Return: void
+ */
+void print_triangle(int size)
+{
+int w, h, hyp;
+/* Going Down */
+for (w = 1; w <= size; w++)
+{
+/* Sideways */
+for (h = 1; h <= size; h++)
+{
+hyp = size - h;
+/* spaces */
+if (hyp > 0 )
+_putchar(' ');
+else
+_putchar('#');
+}
+_putchar('\n');
+}
+}
