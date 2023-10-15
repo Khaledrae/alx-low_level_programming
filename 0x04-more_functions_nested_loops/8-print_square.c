952989893a88c6f18f196b7c490ce7f9fd3d2ae4
Khaledrae <educatejulius@gmail.com>
2023-10-15 12:35:13 -0700

    Weee

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,17 +2,15 @@
 /**
  *void print_square - Prints a square based on given param
  *@size: size of the square
- *
- *Return: void
  */
 void print_square(int size)
 {
 int i, j;
 /* height */
-for (i = 0; i <= size; i++)
+for (i = 0; i < size; i++)
 {
 /* Width */
-for (j = 0; j <= size; j++)
+for (j = 0; j < size; j++)
 {
 _putchar('#');
 }
