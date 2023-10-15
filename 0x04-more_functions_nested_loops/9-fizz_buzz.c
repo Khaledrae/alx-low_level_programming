0d9bb4cfc5cf8b49b4207c8f8b33c6168903bc53
Khaledrae <educatejulius@gmail.com>
2023-10-15 13:03:00 -0700

    Wazi

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,18 +12,22 @@ for (i = 1; i <= 100; i++)
 if (i % 5 == 0 && i % 3 == 0)
 {
 printf("FizzBuzz");
+printf(" ");
 }
 else if (i % 3 == 0)
 {
 printf("Fizz");
+printf(" ");
 }
 else if (i % 5 == 0)
 {
 printf("Buzz");
+printf(" ");
 }
 else
 {
 printf("%d", i);
+printf(" ");
 }
 }
 printf("\n");
