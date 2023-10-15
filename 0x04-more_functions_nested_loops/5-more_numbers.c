531a45d6c5600045f0c4d505ff621b32e99e3ebb
Khaledrae <educatejulius@gmail.com>
2023-10-15 11:53:29 -0700

    Utiaji

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,15 +4,14 @@
  *
  *Return: a void
  */
-void print_most_numbers(void)
+void more_numbers(void)
 {
 int i, j;
 for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 14; j++)
-{
 _putchar('0'+j);
-}
+
 putchar('\n');
 }
 }
