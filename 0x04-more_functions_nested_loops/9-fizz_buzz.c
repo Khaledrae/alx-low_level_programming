72213f5991ea636d35edb980ea834c040900cfc0
Khaledrae <educatejulius@gmail.com>
2023-10-15 12:49:17 -0700

    Eh

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,17 +11,17 @@ for (int i = 1; i <= 100; i++)
 {
 if (i % 3 == 0)
 {
-printf('Fizz');
+printf("Fizz");
 }
 else if (i % 5 == 0)
 {
-printf('Buzz');
+printf("Buzz");
 }
 else if (i % 5 == 0 && i % 3 == 0)
-printf('FizzBuzz');
+printf("FizzBuzz");
 else
 printf("%d", i);
 }
-printf('\n');
+printf("\n");
 return (0);
 }
