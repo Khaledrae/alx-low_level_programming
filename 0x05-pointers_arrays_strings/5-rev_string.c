e548bb41d752b709bc4fb10eb9da1cb14252ea72
Khaledrae <educatejulius@gmail.com>
2023-10-16 20:24:38 -0700

    Weuh 4

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *rev_string - reverses a string from a pointer
+ *@s: the string to print
+ *
+ */
+void print_rev(char *s)
+int l = 0, index;
+char a;
+while (s[index++])
+{
+l++;
+}
+for (index = l - 1; index >= l/2; index--)
+{
+c = s[index];
+s[index] = s[l - index - 1];
+s[l - index - 1] = a;
+}
+*s = a*;
+_putchar('\n');
+}
