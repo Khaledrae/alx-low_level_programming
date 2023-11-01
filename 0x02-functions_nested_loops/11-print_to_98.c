4178c27f5fb2fc905dd85f8061765e0a02d83d21
Khaledrae <educatejulius@gmail.com>
2023-10-31 20:15:56 -0700

    Ewe6

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,20 +9,18 @@ void print_to_98(int n)
 int i;
 if (n >= 98)
 {
-for (i = n; i >= 98; i--)
+for (i = n; i > 98; i--)
 {
-_putchar('0' + i);
-_putchar(',');
-_putchar(' ');
+printf("%d, ", i);
 }
+printf("%d\n", i);
 }
 else
 {
 for (i = n; i <= 98; i++)
 {
-_putchar('0' + i);
-_putchar(',');
-_putchar(' ');
+printf("%d, ", i);
 }
+printf("%d\n", n);
 }
 }
